b8d16e8781d5ba8c47706637c61ca279320bb51a
Eliezozo <sossoumawupenukukpoe228@gmail.com>
2025-03-15 03:14:56 +0000

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+/*let header = document.querySelector('header')
+
+header.addEventListener("mouseover", ()=>{
+    let nom = prompt("Entrez votre nom !!😎") 
+    let score = 1;
+
+    if(score){
+        let BIENVENUE = document.querySelector('BIENVENUE')
+
+        BIENVENUE.classList.add(`${nom}`);
+    }
+})*/
